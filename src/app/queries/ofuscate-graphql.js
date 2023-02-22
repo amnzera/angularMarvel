f50babe1af27c212f83c72f5d407d5dfb30931a5
amn_s <Amnzera160@>
2023-02-22 17:57:21 -0300

    ofuscar

--- src/app/queries/ofuscate-graphql.js
+++ src/app/queries/ofuscate-graphql.js
@@ -1,6 +1,4 @@
 import fs from 'fs';
-"use strict";
-import * as acorn from 'acorn'
 import CryptoJS from 'crypto-js';
 import {GET_PRODUCTS} from "./get-products.query.mjs";
 import {GET_PRODUCTS2} from "./get-products.query.mjs";
@@ -9,64 +7,19 @@ const filePath = new URL(import.meta.url).pathname.substring(1).replace('ofuscat
 
 let arrayConstName = []
 const array = [GET_PRODUCTS,GET_PRODUCTS2]
-let constantes = '';
+let constList = '';
 function encryptQuery() {
 
-  const conteudo = fs.readFileSync(filePath, 'utf8');
-  const constantesName = conteudo.match(/\bconst\s+(\w+)/g);
-  arrayConstName.push(constantesName)
+  const contentFile = fs.readFileSync(filePath, 'utf8');
+  const constName = contentFile.match(/\bconst\s+(\w+)/g);
+  arrayConstName.push(constName)
 
-  // console.log(GET_PRODUCTS)
   array.forEach((teste,index) => {
-    console.log(index)
-    console.log(arrayConstName[0][index])
     const encryptedQuery = CryptoJS.AES.encrypt(teste.loc?.source.body || '', 'secretKey').toString();
-    constantes = constantes + `export ${arrayConstName[0][index]} = \`${encryptedQuery}\`` + '\n';
+    constList = constList + `export ${arrayConstName[0][index]} = \`${encryptedQuery}\`` + '\n';
   })
 
-
-  fs.writeFileSync(filePath, constantes);
-
-
-  // const encryptedQuery = CryptoJS.AES.encrypt(GET_PRODUCTS.loc?.source.body || '', 'secretKey').toString();
-  // const fileContent = `export const GET_PRODUCTS = \`${encryptedQuery}\``;
-  // fs.writeFileSync(filePath, fileContent);
-
-  // ----------------------------------------------------------------
-  // const decrypted = CryptoJS.AES.decrypt(encryptedQuery, 'secretKey');
-  // const originalQuery = decrypted.toString(CryptoJS.enc.Utf8);
-  // const testex = gql(originalQuery)
-  // console.log(testex)
-  // ----------------------------------------------------------------
-
-  // console.log(filePath)
-
-  // //
-  // let concatQuerys = '';
-  // arrayConst.forEach((constData) => {
-  //   const encryptedQuery = CryptoJS.AES.encrypt(constData.loc?.source.body || '', 'secretKey').toString();
-  //   console.log(encryptedQuery)
-  //   concatQuerys = concatQuerys + '\n' + `export const ${constData} = \`${encryptedQuery}\``;
-  // })
-  // console.log(concatQuerys)
-    // fs.writeFileSync(filePath, fileContent);
-
-  // let acorn = require("acorn");
-  // const codigoFonte = fs.readFileSync(filePath, 'utf-8');
-  // console.log(codigoFonte)
-  // const arvoreSintatica = acorn.parse(codigoFonte, { ecmaVersion: 2020 });
-
-  // const consts = [];
-  //
-  // arvoreSintatica.body.forEach((node) => {
-  //   if (node.type === 'VariableDeclaration' && node.kind === 'const') {
-  //     node.declarations.forEach((declaracao) => {
-  //       consts.push(declaracao.id.name);
-  //     });
-  //   }
-  // });
-
-  // console.log(consts);
+  fs.writeFileSync(filePath, constList);
 
 }
 
