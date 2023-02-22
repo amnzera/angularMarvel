65b4f50c8c322eb13ba4cdd17410e3030ac090b8
amn_s <Amnzera160@>
2023-02-22 18:00:41 -0300

    ofuscar

--- src/app/queries/ofuscate-graphql.js
+++ src/app/queries/ofuscate-graphql.js
@@ -3,10 +3,10 @@ import CryptoJS from 'crypto-js';
 import {GET_PRODUCTS} from "./get-products.query.mjs";
 import {GET_PRODUCTS2} from "./get-products.query.mjs";
 
-const filePath = new URL(import.meta.url).pathname.substring(1).replace('ofuscate-graphql.js','get-products.query.mjs');
+const arrayImports = [GET_PRODUCTS,GET_PRODUCTS2]
 
+const filePath = new URL(import.meta.url).pathname.substring(1).replace('ofuscate-graphql.js','get-products.query.mjs');
 let arrayConstName = []
-const array = [GET_PRODUCTS,GET_PRODUCTS2]
 let constList = '';
 function encryptQuery() {
 
@@ -14,7 +14,7 @@ function encryptQuery() {
   const constName = contentFile.match(/\bconst\s+(\w+)/g);
   arrayConstName.push(constName)
 
-  array.forEach((teste,index) => {
+  arrayImports.forEach((teste,index) => {
     const encryptedQuery = CryptoJS.AES.encrypt(teste.loc?.source.body || '', 'secretKey').toString();
     constList = constList + `export ${arrayConstName[0][index]} = \`${encryptedQuery}\`` + '\n';
   })
