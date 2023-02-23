95a7bff80eb594c7cbf607a5982e7a33d657d540
amn_s <Amnzera160@>
2023-02-23 11:25:45 -0300

    ofuscar 2

--- src/app/queries/get-products.query.ts
+++ src/app/queries/get-products.query.ts
@@ -1,2 +1,34 @@
-export const GET_PRODUCTS = `U2FsdGVkX1/ptQAnMGnXihAoeBCRJccmWrbPUTuvDgzGLLFkRop6BsjwXjM9ILIA1uNnC12YWXtrVA3MmnNYheHK9z8z1/DKPmGDEqS1FiNyZwW7a7nW6cLSc98Jnd4jGosAL8t0+QS6cOtyeEngQ2t5rvr1YhroElMn3TxrnPLNmt5m383V1qFWundwxVDyw8G7Vm72R6eDx4PxCPeqxkr0anaxMFW6qjgaNjHeBw9v1taL7cGNLi2rBjCfHTIjyKarcxcaW5gGg9JqCaDhU/DlPvPMQV7FLKzvLfQ8VOqHstmuuq0Fxy76TDoZ7ZgjUPkieyLYIWretFMOhXY+Tx/jPZvETC0KmdAcDLLi/u6Cum6qH5J78srFrXDspe0hRMSxh2UPLtTQhXdHReDcKg==`
-export const GET_PRODUCTS2 = `U2FsdGVkX19CWkkagFb+hP3DuAsPoIqA8UJw1t6TOxyDTTIqfF8+T/JcYMLkwjk32K55OOq3IO64BHgwWLpcKdXHL7eRP4xMeMCU17w6fbpjQallykM/Ei1TUVGT05jlnj/1jmVq6w0x1AknnkpfVBSo98bN0cEi2PmMr3ucTcsGenyKPEt60yK+9zM6UvT6`
+import {gql} from "graphql-tag";
+
+export const GET_PRODUCTS = gql`
+  query Query {
+    allFilms {
+      films {
+        title
+        director
+        releaseDate
+        speciesConnection {
+          species {
+            name
+            classification
+            homeworld {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_PRODUCTS2 = gql`
+  query Query {
+    allFilms {
+      films {
+        title
+        director
+        releaseDate
+      }
+    }
+  }
+`;
