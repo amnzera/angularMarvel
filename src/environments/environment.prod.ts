a9b7f149b2b59ded66842581c88e3c2a007e0ecd
amnzera <amnzera@users.noreply.github.com>
2023-07-30 15:48:56 +0000

    deploy: 6f02441659ebfd32c00e5ba06f694ffa5008452f

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  urlApi: "http://gateway.marvel.com/v1/public/",
-  apikey: "a83d8314698cb126d6e37701215f284a",
-  hash: "118ed793f750d7b8212874e99f8495f3",
-  ts: "1600905709760"
-};
